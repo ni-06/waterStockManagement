a12501e6214644f6a160e36bbe2ce4cf8e0195ab
jahiduliemonIT <jahidulislamdm@gmail.com>
2026-09-04 11:04:11 +0600

    sd

--- waterallocationsystem-jahid.c
+++ waterallocationsystem-jahid.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int main() 
-{
-    printf("Hello, World!");
-    printf("Its Emon");
-    return 0;
-}
+
+#define MAX_ZONES 6
+#define MAX_NAME 30
+double water_allocations[MAX_ZONES];
+int zone_count = 0;
