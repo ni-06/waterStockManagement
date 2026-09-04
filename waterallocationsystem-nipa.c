96bbcf1f2f1749c3b8409ec39563ba4d1c92f4cb
jahiduliemonIT <jahidulislamdm@gmail.com>
2026-09-04 11:18:38 +0600

    Add main function and zone input logic

--- waterallocationsystem-nipa.c
+++ waterallocationsystem-nipa.c
@@ -4,3 +4,8 @@
 #define MAX_NAME 30
 double water_allocations[MAX_ZONES];
 int zone_count = 0;
+int main() {
+printf("Enter the number of zones (max %d): ", MAX_ZONES);
+scanf("%d", &zone_count);
+return 0;
+}
